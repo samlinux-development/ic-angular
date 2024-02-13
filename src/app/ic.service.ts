40e917cfb2c919e4addcd5940c98b45c63fd8e65
Roland Bole <rbole@samlinux.at>
2024-02-13 11:09:39 +0100

    add new ic actor service to get ride of ready to use actor

--- src/app/ic.service.ts
+++ src/app/ic.service.ts
@@ -1,20 +1,58 @@
 import { Injectable } from '@angular/core';
-import { createActor } from '../declarations/backend';
+import { Actor, ActorSubclass, HttpAgent } from "@dfinity/agent";
 import { environment } from '../environments/environment';
+
+import type { _SERVICE } from "../declarations/backend/backend.did";
+
+//@ts-ignore
+import {idlFactory} from "../declarations/backend/backend.did.js";
 
 @Injectable({
   providedIn: 'root'
 })
 export class IcService {
-  constructor() { }
-  public async greet(name:string){
-    // Create an actor to interact with the IC for a particular canister ID
-    const canisterId = environment.BACKEND_CANISTER_ID;
-    const actor = createActor(canisterId, { agentOptions: { } });
-    
-    console.log('check environment', environment);
+  private readonly CANISTER_ID_BACKEND = environment.BACKEND_CANISTER_ID;
+  private readonly DFX_NETWORK = environment.DFX_NETWORK;
+  private readonly DFX_HOST = environment.DFX_HOST;
+  private Actor: any = undefined;
 
-    return await actor.greet(name);
+  constructor() {}
+
+  private async createActor(options:any): Promise<ActorSubclass<_SERVICE>> {
+
+    if(this.CANISTER_ID_BACKEND == undefined){
+      throw new Error()
+    }
+    options.agentOptions = {};
+    options.agentOptions.host = this.DFX_HOST;
+
+    const agent = options.agent || new HttpAgent({ ...options.agentOptions });
+
+    // Fetch root key for certificate validation during development
+    if (this.DFX_NETWORK !== "ic") {
+      agent.fetchRootKey().catch((err:Error) => {
+        console.warn(
+          "Unable to fetch root key. Check to ensure that your local replica is running"
+        );
+        console.error(err);
+      });
+    }
     
+    const actor = await Actor.createActor<_SERVICE>(idlFactory, {
+      agent,
+      canisterId: this.CANISTER_ID_BACKEND
+    });
+    return actor;
+  };
+
+  public async greet(name:string){
+    // Create an actor to interact with the IC if it doesn't exist
+    if(this.Actor == undefined){
+      console.log('Creating actor one time');
+      this.Actor = await this.createActor({});
+    }
+    
+    console.log('canisterId:', this.CANISTER_ID_BACKEND);
+    return await this.Actor.greet(name);
   }
 }
